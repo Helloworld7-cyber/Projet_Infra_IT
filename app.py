64b476bf7633a5051d4cc508a333607b42f78c18
Helloworld7-cyber <Matteobray77@gmail.com>
2026-01-21 09:40:01 +0100

    Add index route to display all books

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    livres = conn.execute('SELECT * FROM livres').fetchall()
+    conn.close()
+    return render_template('bibliotheque.html', livres=livres)
+    
 @app.route('/fiche_nom/<nom>')
 def fiche_nom(nom):
     auth = request.authorization
     if not auth or auth.username != 'user' or auth.password != '12345':
         return Response('Login requis', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     # ... code de recherche ...
+
 @app.route('/disponibles')
 def livres_disponibles():
     conn = get_db_connection()
